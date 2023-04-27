7ac0867c5753ce4b268b8015bb3c798490636fc8
Andreas Zerbst <andr317c@live.dk>
2023-04-27 11:38:17 +0200

    Added version 9 and updated v10 to the newest verison

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -37,12 +37,17 @@ variable "umbraco_cms_versions" {
     version_10 = {
       version_name    = "v10"
       dotnet_version  = "v6.0"
-      umbraco_version = "10.3.1"
+      umbraco_version = "10.5.0"
     }
     version_11 = {
       version_name    = "v11"
       dotnet_version  = "v7.0"
       umbraco_version = "11.3.0"
+    }
+    version_9 = {
+      version_name    = "v9"
+      dotnet_version  = "v5.0"
+      umbraco_version = "9.5.4"
     }
   }
 }
