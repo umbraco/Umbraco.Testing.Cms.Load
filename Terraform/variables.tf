1e767a47c9c0901af16b64b0608726973d8e3e0e
Andreas Zerbst <andr317c@live.dk>
2023-06-02 11:35:26 +0200

    Updated the variables
    Removed the default values for terraform.

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -33,17 +33,7 @@ variable "tenant_id" {
 # These are the umbraco versions being load tested. You can update, add more or delete these if you want to test other versions
 variable "umbraco_cms_versions" {
   type = map(object({
-    dotnet_version  = string
+    dotnet_version = string
     umbraco_version = string
-  }))
-  default = {
-    version_10_5_1 = {
-      dotnet_version  = "v6.0"
-      umbraco_version = "10.5.1"
-    }
-    version_11_3_1 = {
-      dotnet_version  = "v7.0"
-      umbraco_version = "11.3.1"
-    }
-  }
+  })) 
 }
